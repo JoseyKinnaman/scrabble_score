c5ef5fa8160da81bfef3830feafa4ab3d382d403
Jozy Kinnaman <jozypants@gmail.com>
2020-05-05 16:31:08 -0700

    refactored hash scrabble_score

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -1,26 +1,42 @@
 require ('pry')
+
 class Scrabble
-
-  # def initialize(str)
-  #   @word = str
-  # end
+  def initialize(word)
+    @word = word
+  end
   
-    
-    def character_check(word)
-      output = true
-      if (word.class != String)
-        puts ("Please enter a word, I can not turn numbers into words.")
-         output = false
-      else
-         puts("yeah, good!")
-        #  output = self.score(word)
+  
+  def word_score
+    scrabble_score = {"A" => 1 ,"B" => 3, "C" => 3, "D" => 2, "E" => 1, "F" => 4, "G" =. 2, "H" => 4, "J" => 8 ,"K" => 5, "L"  => 1, "M" =>, "P"] , 8 => [,"X"], "V" => 4, "W" => 4, "X" => 8, "Y" => 4, "Z" = 10}.invert
+    word_split = @word.split("")
+    total_score = []
+   
+    word_split.each do |letter|
+        total_score.push(scrabble_score.fetch([letter]))
       end
-      output
     end
-
-
-  def score()
-    scrabble_score = {1 => ["A","E","I", "O", "U", "L", "N", "R", "S", "T"] , 2 => ["D", "G"], 3 => ["B", "C", "M", "P"], 4 => ["F", "H", "V", "W", "Y"], 5 => ["K"], 8 => ["J","K"], 10 => ["Q", "Z"]}
+    total_score.sum
   end
 end
 
+
+    # turn word to a string
+    # index through a string
+    # compare letters of a string to values of keys 1, 2, 3 , 4, 5, 8, 10
+
+       #  if score.each do |key, value|
+        #   score.values.includes?(letter)
+          
+        # end
+
+          # def character_check()
+  #   output = true
+  #   if (@word != a..z)
+  #     puts ("Please enter a word, I can not turn numbers into words.")
+  #     output = false
+  #   else
+  #     puts("yeah, good!")
+  #     #  output = self.score(word)
+  #   end
+  #   output
+  # end
