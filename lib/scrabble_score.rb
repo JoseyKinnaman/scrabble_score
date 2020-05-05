a5d35db9ed72ea1d2347615eccb3a74f3d60dafb
Jozy Kinnaman <jozypants@gmail.com>
2020-05-05 11:47:12 -0700

    update scrable_score

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -1,16 +1,26 @@
+require ('pry')
 class Scrabble
 
-  def initialize(str)
-    @word = str
-  end 
-
-  def character_check
-    if (@word.match?(/\A-?\d+\Z/)
-      puts ("Please enter a word, I can not turn numbers into words.")
-    end
+  # def initialize(str)
+  #   @word = str
+  # end
+  
     
+    def character_check(word)
+      output = true
+      if (word.class != String)
+        puts ("Please enter a word, I can not turn numbers into words.")
+         output = false
+      else
+         puts("yeah, good!")
+        #  output = self.score(word)
+      end
+      output
+    end
+
+
   def score()
-    scrabble_score = {1 => ["A","E","I"], 5 => ["Z"]}
+    scrabble_score = {1 => ["A","E","I", "O", "U", "L", "N", "R", "S", "T"] , 2 => ["D", "G"], 3 => ["B", "C", "M", "P"], 4 => ["F", "H", "V", "W", "Y"], 5 => ["K"], 8 => ["J","K"], 10 => ["Q", "Z"]}
   end
 end
 
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -1,14 +1,16 @@
 require ('rspec')
 require ('scrabble_score')
 
-describe("#scrabble_score") do
+describe("Scrabble#character_check") do
+it("returns an error message if the user enters a number") do
+  my_scrabble = Scrabble.new()
+  expect(my_scrabble.character_check(1)).to(eq(false))
+  end
+end
+
+describe("Scrabble#score") do
   it("returns a scrabble score for the letter 'a'") do
     my_scrabble = Scrabble.new()
-    expect(my_scrabble.score.fetch("1")).to(eq("A","E","I"))
-    end
-    describe("#scrabble_score") do
-    it("returns an error message if the user enters a number")
-    my_scrabble = Scrabble.new()
-    expect(my_scrabble.character_check(1)).to(eq(true))
+    expect(my_scrabble.score.fetch(5)).to(eq(["K"]))
     end
   end
